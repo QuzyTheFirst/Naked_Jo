d695e1d0ecb45e67f6fc25ff5be73f3c57c4f7ef
Quzy <40630448+QuzyTheFirst@users.noreply.github.com>
2023-03-18 18:08:17 +0200

    Update 0.3.7
    Finished sorting tool
    
    Sorted all levels

--- Assets/Scripts/Editor/SortingWindow.cs
+++ Assets/Scripts/Editor/SortingWindow.cs
@@ -1,25 +1,71 @@
 using UnityEngine;
 using UnityEditor;
 
+[System.Serializable]
+public class SortingItem
+{
+    public string FolderName;
+    public string[] Tags;
+}
+
 public class SortingWindow : EditorWindow
 {
+    public SortingItem[] Items;
+    SerializedObject so;
+
     [MenuItem("Window/MyWindows/Sorting")]
     public static void ShowWindow()
     {
         GetWindow<SortingWindow>("Sorting");
     }
 
+    private void OnEnable()
+    {
+        ScriptableObject target = this;
+        so = new SerializedObject(target);
+    }
+
     private void OnGUI()
     {
         GUILayout.Label("Sort your level!");
 
+        so.Update();
+        SerializedProperty itemsProperty = so.FindProperty("Items");        
+
+        EditorGUILayout.PropertyField(itemsProperty, true);
+        so.ApplyModifiedProperties();
+
+        ButtonSort();
+    }
+
+    private void ButtonSort()
+    {
         if (GUILayout.Button("Sort!"))
         {
             GameObject[] allObjects = FindObjectsOfType<GameObject>();
 
+            GameObject[] selectedObject = Selection.gameObjects;
+
             GameObject mainFolder = CreateOrFindFolder("Sorted Folder", allObjects);
 
+            foreach(SortingItem item in Items)
+            {
+                GameObject folder = CreateOrFindFolder(item.FolderName, allObjects);
 
+                folder.transform.parent = mainFolder.transform;
+
+                foreach(GameObject obj in selectedObject)
+                {
+                    foreach(string tag in item.Tags)
+                    {
+                        if (obj.name.ToLower().Contains(tag.ToLower())){
+                            obj.transform.parent = folder.transform;
+                        }
+                    }
+                }
+            }
+
+            SaveChanges();
         }
     }
 
